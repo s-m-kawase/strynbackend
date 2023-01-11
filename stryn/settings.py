7ac576426a9d91532c9c46fd72ae018cf563bc34
brunobarross <altamiro_bruno@yahoo.com.br>
2023-01-11 09:18:29 -0300

    consertando cors

--- stryn/settings.py
+++ stryn/settings.py
@@ -22,14 +22,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'core',
     'pagamentos',
     'pedidos',
     'rest_framework',
     'drf_spectacular',
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -37,6 +40,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:9000',
 ]
 
 ROOT_URLCONF = 'stryn.urls'
