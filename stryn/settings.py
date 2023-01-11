8178012bb74453c868391e3902c50c0a484b1a1c
paulinaJulia <paulinajulia2015@gmail.com>
2023-01-11 10:02:39 -0300

    subindo servidor

--- stryn/settings.py
+++ stryn/settings.py
@@ -13,6 +13,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = [
     'localhost',
     'stryn.herokuapp.com',
+    'stryn.dokku.outboxsistemas.com',
 ]
 
 INSTALLED_APPS = [
@@ -75,6 +76,28 @@ DATABASES = {
         'DATABASE_URL', default=default_dburl, cast=dburl
     )
 }
+
+USE_AWS = config('USE_AWS', default=False, cast=bool)
+if USE_AWS:
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'django-stryn' 
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_LOCATION = 'static'
+    AWS_DEFAULT_ACL = None
+
+    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, PUBLIC_MEDIA_LOCATION)
+    DEFAULT_FILE_STORAGE = 'stryn.storage_backends.PublicMediaStorage' 
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
