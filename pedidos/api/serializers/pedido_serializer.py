e2314fbc3e77db346b281ba38f65e3c5298423e1
diovan baptista <diovantrab@gmail.com>
2023-01-27 12:03:04 -0300

    ajuste serializers stryn

--- pedidos/api/serializers/pedido_serializer.py
+++ pedidos/api/serializers/pedido_serializer.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers 
 from pedidos.models import Pedido
 
+from pagamentos.api.serializers.pagamento_serializers import PagamentoSerializer
+from pagamentos.api.serializers.cupom_serializers import CupomSerializer
+from .cliente_serializers import ClienteSerializer
+from .tempo_estimado_serializer import TempoEstimadoSerializer
 
 class PedidoSerializer((serializers.ModelSerializer)):
+    pagamento = PagamentoSerializer()
+    cupom =CupomSerializer()
+    cliente = ClienteSerializer()
+    tempo_estimado = TempoEstimadoSerializer()
     class Meta:
         model = Pedido
         fields = '__all__'
