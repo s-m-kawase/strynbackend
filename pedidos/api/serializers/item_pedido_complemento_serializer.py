a1c82cc7af311226d52c9d99bbb82dcec5a8edd1
diovan baptista <diovantrab@gmail.com>
2023-02-03 12:29:11 -0300

    ajuste das apis

--- pedidos/api/serializers/item_pedido_complemento_serializer.py
+++ pedidos/api/serializers/item_pedido_complemento_serializer.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 from pedidos.models import ItensPedidoComplementos
+from .item_pedido_serializer import ItensPedidoSerializer
+from .complemento_serializer import ComplementosSerializer
 
 
 class ItensPedidoComplementosSerializer(serializers.ModelSerializer):
+    complemento_read = serializers.SerializerMethodField()
+    item_pedido_read = serializers.SerializerMethodField()
+
     total=serializers.ReadOnlyField()
+
+
+    def get_item_pedido_read(self,obj):
+        return ItensPedidoSerializer(instance=obj.item_pedido).data
+
+    def get_complemento_read(self,obj):
+        return ComplementosSerializer(instance=obj.complemento).data
+
     class Meta:
         model = ItensPedidoComplementos
         fields = '__all__'
