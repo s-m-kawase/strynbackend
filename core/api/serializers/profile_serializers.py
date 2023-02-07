64a94ce36ea814164de9bba338222533b4a4a71a
diovan baptista <diovantrab@gmail.com>
2023-02-07 10:54:52 -0300

    filtros da tela de pedidos

--- core/api/serializers/profile_serializers.py
+++ core/api/serializers/profile_serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers 
 from core.models import Profile
+from pedidos.api.serializers.restaurante_serializer import RestauranteSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
+    restaurante_read = serializers.SerializerMethodField()
+
+    def get_restaurante_read(self, obj):    
+        return RestauranteSerializer(instance=obj.restaurante).data
+
+
     class Meta:
         model = Profile
         fields = '__all__'
--- pedidos/api/serializers/pedido_serializer.py
+++ pedidos/api/serializers/pedido_serializer.py
@@ -3,6 +3,7 @@ from pedidos.models import Pedidos
 
 from .cliente_serializers import ClienteSerializer
 from .tempo_estimado_serializer import TempoEstimadoSerializer
+from .restaurante_serializer import RestauranteSerializer
 
 class PedidosSerializer(serializers.ModelSerializer):
     #  property
@@ -11,14 +12,17 @@ class PedidosSerializer(serializers.ModelSerializer):
     
     cliente_read = serializers.SerializerMethodField()
     tempo_estimado_read = serializers.SerializerMethodField()
+    restaurante_read = serializers.SerializerMethodField()
     
 
     def get_tempo_estimado_read(self, obj):
          return [TempoEstimadoSerializer(instance=tempo_estimado).data for tempo_estimado in obj.tempo_estimado.all()]
 
-
     def get_cliente_read(self, obj):    
         return ClienteSerializer(instance=obj.cliente).data
+
+    def get_restaurante_read(self, obj):    
+        return RestauranteSerializer(instance=obj.restaurante).data
 
     class Meta:
         model = Pedidos
