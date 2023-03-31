a6ca9968be873d1249f77262c157fb1bd6890a10
cleocardoso <cleotads21@gmail.com>
2023-03-31 09:34:29 -0300

    ajustes na categoria cardapio

--- pedidos/api/viewsets/categoria_cadarpio_viewset.py
+++ pedidos/api/viewsets/categoria_cadarpio_viewset.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, filters
 import django_filters.rest_framework
+from pedidos.models.cardapio import Cardapio
 from pedidos.models import CategoriaCardapio
 from ..serializers.categoria_cardapio_serializer import *
 from rest_framework.permissions import IsAuthenticated
@@ -19,6 +20,21 @@ class CategoriaCardapioViewSet(viewsets.ModelViewSet):
 
     filter_backends = [filters.SearchFilter, django_filters.rest_framework.DjangoFilterBackend]
 
-    filterset_fields = ['status']
+    filterset_fields = ['status','restaurante']
 
     search_fields = ['nome']
+
+    def get_queryset(self):
+        query = super().get_queryset()
+        if self.request.GET.get("meus_cardapios",False):
+            usuario = self.request.user
+            cardapios = Cardapio.objects.filter(restaurante__usuario=usuario)
+            ids_categorias = []
+            for cardapio in cardapios:
+                for categoria in cardapio.categorias.all():
+                    ids_categorias.append(categoria.id)
+            ids_categorias = list(set(ids_categorias))
+
+            query = query.filter(id__in=ids_categorias)
+        
+        return query
