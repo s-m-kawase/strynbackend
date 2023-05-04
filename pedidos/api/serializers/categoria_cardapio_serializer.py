60fa5dde9572ca0c35159ac90eceafc4fe2fc603
diovan baptista <diovantrab@gmail.com>
2023-05-04 10:18:43 -0300

    ajuste para categorias ordem

--- pedidos/api/serializers/categoria_cardapio_serializer.py
+++ pedidos/api/serializers/categoria_cardapio_serializer.py
@@ -1,17 +1,14 @@
 from rest_framework import serializers 
 from pedidos.models import CategoriaCardapio
-from .ordem_categoria_serializers import OrdemCategoriaCardapioSerializer
-from pedidos.models.ordem_categoria_cardapio import OrdemCategoriaCardapio
+
 
 
 
 class CategoriaCardapioSerializer(serializers.ModelSerializer):
-    ordens = serializers.SerializerMethodField()
+    
 
     class Meta:
         model = CategoriaCardapio
         fields = '__all__'
 
-    def get_ordens(self, obj):
-        ordens = OrdemCategoriaCardapio.objects.filter(categoria=obj).order_by('ordem')
-        return OrdemCategoriaCardapioSerializer(ordens, many=True).data
+   
--- pedidos/api/serializers/ordem_categoria_serializers.py
+++ pedidos/api/serializers/ordem_categoria_serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers 
 from ...models.ordem_categoria_cardapio import OrdemCategoriaCardapio
+from .categoria_cardapio_serializer import CategoriaCardapioSerializer
 
 
 class OrdemCategoriaCardapioSerializer(serializers.ModelSerializer):
+    categoria_read = serializers.SerializerMethodField()
+    
+    def get_categoria_read(self, obj):
+        return CategoriaCardapioSerializer(obj.categoria).data
     class Meta:
         model = OrdemCategoriaCardapio
         fields = '__all__'
