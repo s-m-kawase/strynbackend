490cf248846f2793e08ecb28c652180f3c611d02
Diovan <diovantrab@gmail.com>
2023-06-13 14:01:15 -0300

    ajuste reembolso

--- pedidos/api/viewsets/pedido_viewset.py
+++ pedidos/api/viewsets/pedido_viewset.py
@@ -148,7 +148,7 @@ class PedidosViewSet(viewsets.ModelViewSet):
         # Redireciona para a URL do checkout do Stripe
         return Response({'checkout_url': checkout_session.url, 'session_id': checkout_session.id})
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['get'])
     def solicitar_reembolso(self, request, pk):
         pedido = Pedidos.objects.get(id=pk)
 
