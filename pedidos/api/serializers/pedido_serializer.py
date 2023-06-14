6707e7068e2459474ededc4ca432f936b994f66f
Diovan <diovantrab@gmail.com>
2023-06-14 08:27:15 -0300

    ajeste no que altamiro pediu

--- pedidos/api/serializers/pedido_serializer.py
+++ pedidos/api/serializers/pedido_serializer.py
@@ -62,7 +62,7 @@ class PedidosSerializer(serializers.ModelSerializer):
         return CupomSerializer(instance=obj.cupom).data if obj.cupom else None
 
     def get_tempo_estimado_read(self, obj):
-         return [TempoEstimadoSerializer(instance=tempo_estimado).data for tempo_estimado in obj.tempo_estimado.all()]
+         return [TempoEstimadoSerializer(instance=obj.tempo_estimado).data] if obj.tempo_estimado else None
 
     # def get_cliente_read(self, obj):
     #     return ClienteSerializer(instance=obj.cliente).data if obj.cliente else None
