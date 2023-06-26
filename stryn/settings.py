9351c19b666940043b182dcb46b7f974c39bc729
Diovan <diovantrab@gmail.com>
2023-06-26 14:41:31 -0300

    configuração aws padrão

--- stryn/settings.py
+++ stryn/settings.py
@@ -105,27 +105,27 @@ else:
 
 USE_AWS = config('USE_AWS', default=False, cast=bool)
 if USE_AWS:
-    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'django-stryn2'
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = "django-stryn2"
+    AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
+        "CacheControl": "max-age=86400",
     }
-    AWS_LOCATION = 'static'
+    AWS_LOCATION = "static"
     AWS_DEFAULT_ACL = None
 
-    #STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    STATIC_HOST = "https://d120psd45kloyc.cloudfront.net"
-    STATIC_URL = STATIC_HOST + "/static/"
+    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # s3 public media settings
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, PUBLIC_MEDIA_LOCATION)
-    DEFAULT_FILE_STORAGE = 'stryn.storage_backends.PublicMediaStorage'
+    PUBLIC_MEDIA_LOCATION = "media"
+    MEDIA_URL = "https://%s/%s/" % (
+        AWS_S3_CUSTOM_DOMAIN,
+        PUBLIC_MEDIA_LOCATION,
+    )
+    DEFAULT_FILE_STORAGE = "stryn.storage_backends.PublicMediaStorage"
 
 
 AUTH_PASSWORD_VALIDATORS = [
