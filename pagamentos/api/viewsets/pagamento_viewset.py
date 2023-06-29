091040fbf422fbfa7546019b45b4e33e78342779
Diovan <diovantrab@gmail.com>
2023-06-29 09:53:25 -0300

    ajuste nas funções do sql

--- pagamentos/api/viewsets/pagamento_viewset.py
+++ pagamentos/api/viewsets/pagamento_viewset.py
@@ -152,7 +152,7 @@ class PagamentoViewSet(viewsets.ModelViewSet):
                         ON pag.pedido_id = ped.id
                         LEFT JOIN pagamentos_cupom cup
                         ON ped.cupom_id = cup.id
-                        WHERE to_char(ped.data_criacao::date, 'DD/MM/YYYY') = '{mesano}'
+                        WHERE to_char(ped.data_criacao::date, 'DD/MM/YYYY') = TO_CHAR('{mesano}'::date,'YYYY/MM')
                     """
 
         with connection.cursor() as cursor:
@@ -186,7 +186,7 @@ class PagamentoViewSet(viewsets.ModelViewSet):
                         ON pag.pedido_id = ped.id
                         LEFT JOIN pagamentos_cupom cup
                         ON ped.cupom_id = cup.id
-                        WHERE to_char(ped.data_criacao::date, 'YYYY/MM') = '{mesano}'
+                        WHERE to_char(ped.data_criacao::date, 'YYYY/MM') = TO_CHAR('{mesano}'::date,'YYYY/MM')
                         GROUP BY ped.data_criacao_f,data_criacao2
                         ORDER BY ped.data_criacao_f DESC
                     """
