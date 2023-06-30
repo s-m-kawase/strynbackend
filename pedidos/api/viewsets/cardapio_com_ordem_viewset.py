ad0aca35d40b88c73ffa913a93e1ae1d4a2168ff
Diovan <diovantrab@gmail.com>
2023-06-30 09:57:41 -0300

    ajuste pegando ordem

--- pedidos/api/viewsets/cardapio_com_ordem_viewset.py
+++ pedidos/api/viewsets/cardapio_com_ordem_viewset.py
@@ -1,14 +1,21 @@
 from rest_framework import filters, viewsets
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.permissions import BasePermission
 from ...models import Cardapio
 from ..serializers.cardapio_com_ordem_serializer import CardapioComOrdemSerializer
 
 
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        return request.user and request.user.is_superuser
+
 class CardapioComOrdemViewSet(viewsets.ModelViewSet):
     queryset = Cardapio.objects.all()
     serializer_class = CardapioComOrdemSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminOrReadOnly,]
 
     filter_backends = [filters.SearchFilter]
 
@@ -23,7 +30,7 @@ class CardapioComOrdemViewSet(viewsets.ModelViewSet):
         parametro = self.request.query_params
         cardapio = parametro.get('cardapio',None)
         restaurante = parametro.get('restaurante',None)
-        
+
 
         # Verifica se o usuário é anônimo
         if self.request.user.is_authenticated:
