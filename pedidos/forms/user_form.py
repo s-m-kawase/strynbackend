a3ed09aff6d3af7334067a0188a813a6debb66cd
Diovan <diovantrab@gmail.com>
2023-08-07 09:06:23 -0300

    tirei a obrigatoriedade de username

--- pedidos/forms/user_form.py
+++ pedidos/forms/user_form.py
@@ -12,5 +12,5 @@ class UserForm(forms.ModelForm):
         fields = ['username','password']
 
     def __init__(self, *args, **kwargs):
-        '''Método para executarmos ações ao iniciar a classe.'''
         super(UserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].required = False
