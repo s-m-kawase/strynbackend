35eca30c27a7360b2bc2b2b1d0806731a18a51c6
Diovan <diovantrab@gamil.com>
2023-09-27 09:25:29 -0300

    testar webhok asaas

--- pedidos/api/viewsets/webhook_asaas.py
+++ pedidos/api/viewsets/webhook_asaas.py
@@ -1,5 +1,4 @@
 import stripe
-from django.conf import settings
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -11,17 +10,69 @@ from emails.models import MensagemEmail, TemplateEmail
 from collections import namedtuple
 from django.http import JsonResponse
 from django.utils import timezone
+from django.views import View
+import hmac
+import hashlib
 
-
-stripe.api_key = config('ASAAS_API_KEY')
+asaas_api = config('ASAAS_API_KEY')
 
 class AsaasWebhookViewSet(ViewSet):
 
-
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=['post'])
     @csrf_exempt
     def webhook(self, request):
+        # Obtenha os dados do webhook do corpo da solicitação
+        payload = request.data
 
-        endpoint_secret = config('ASAAS_API_SECRET')
-        payload = request.body
-        event = None
+        # Verifique se o evento é um evento válido do Asaas
+        event_type = payload.get('event_type')
+
+        if event_type == 'PAYMENT_CREATED':
+            # Lide com o evento de pagamento criado aqui
+            # Acesse os dados do pagamento em payload['payment_data']
+            # Faça o processamento necessário e atualize seu sistema
+            pedido_id = payload['payment_data']['pedido_id']
+            self.update_pedido_status(pedido_id)
+
+        
+
+        return JsonResponse({'message': 'Webhook recebido com sucesso'})
+
+    def update_pedido_status(self, pedido_id):
+        pedido = Pedidos.objects.get(id=pedido_id)
+        pedido.status_pedido = 'Pago'
+        pedido.hora_status_pago = timezone.now()
+        pedido.save()
+        
+
+
+        
+
+    # @csrf_exempt
+    # def post(self, request):
+    #     endpoint_secret = config('ASAAS_API_SECRET')
+    #     secret_key = bytes(endpoint_secret, 'utf-8')
+        
+    #     # Verifique a assinatura do webhook para garantir que os dados não foram adulterados
+    #     received_signature = request.headers.get('X-Asaas-Signature')
+    #     computed_signature = hmac.new(secret_key, request.body, hashlib.sha256).hexdigest()
+
+    #     if received_signature != computed_signature:
+    #         return JsonResponse({'message': 'Assinatura inválida'}, status=400)
+
+    #     # O webhook é válido, você pode processar os dados aqui
+    #     data = request.body.decode('utf-8')
+    #     # Faça o processamento necessário com os dados do webhook
+    #     if 'event' in data and data['event'] == 'PAYMENT_CREATED':
+    #         payment_data = data['data']  # Suponha que os dados do pagamento estejam no campo 'data'
+    #         pedido_id = payment_data['pedido_id']  # Suponha que o ID do pedido esteja nos dados do pagamento
+    #         novo_status = 'Pago'  # Suponha que você queira atualizar para o status 'Pago'
+
+    #         pedido = update_pedido_status(pedido_id, novo_status)
+    #         if pedido:
+    #             # Pedido atualizado com sucesso
+    #             # Você pode retornar uma resposta de sucesso aqui
+    #         else:
+    #             # Pedido não encontrado
+    #             # Retorne uma resposta de erro, se apropriado
+    #     return JsonResponse({'message': 'Webhook recebido com sucesso'})
