bb3fb82058cb0aac65b637dcc17cf9fd1dab79cb
Diovan <diovantrab@gamil.com>
2024-01-12 16:12:32 -0300

    trazer pemissao

--- core/api/serializers/user_serializer.py
+++ core/api/serializers/user_serializer.py
@@ -4,16 +4,17 @@ from .profile_serializers import ProfileSerializer
 class UserSerializer(serializers.ModelSerializer):
     #profile = ProfileSerializer()
     groups = serializers.SerializerMethodField()
-    
+    permissions_by_user = serializers.SerializerMethodField()
+
     def get_groups(self, obj):
         return [group.name for group in obj.groups.all()]
             
     def get_permissions_by_user(self, obj):
         permissoes =[] 
-        for permissao in obj.user_permissions.values_list('name',flat = True):
+        for permissao in obj.user_permissions.values_list('id',flat = True):
             permissoes.append(permissao)
         for group in obj.groups.all():
-            for permissao in group.permissions.all().values_list('name',flat = True):
+            for permissao in group.permissions.all().values_list('id',flat = True):
                 permissoes.append(permissao)
 
         return list(set(permissoes))
