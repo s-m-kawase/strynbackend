2e487c739cf4df8c1aabe0f75cff00d20a0b0643
Diovan <diovantrab@gamil.com>
2024-02-19 14:57:27 -0300

    ajuste mes

--- pedidos/api/viewsets/pedido_viewset.py
+++ pedidos/api/viewsets/pedido_viewset.py
@@ -495,8 +495,13 @@ class PedidosViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'], detail=False)
     def select_data(self,request):
-        sql_query = f"""  SELECT DISTINCT to_char( data_criacao::date, 'FMMonthYYYY')
-                          FROM pedidos_pedidos
+        sql_query = f"""  SELECT month_with_year
+                            FROM (
+                                SELECT DISTINCT to_char(data_criacao::date, 'FMMonthYYYY') AS month_with_year, MIN(data_criacao) AS min_date
+                                FROM pedidos_pedidos
+                                GROUP BY month_with_year
+                            ) AS subquery
+                            ORDER BY min_date desc;
                               """
 
         with connection.cursor() as cursor:
