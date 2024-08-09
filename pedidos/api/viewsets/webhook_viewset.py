1a45c0a5471566e155927015d12533eeae7fc722
Emanuel-Bruno <emanuelbruno2018vasc@gmail.com>
2024-08-08 21:38:56 -0300

    Testando novo split

--- pedidos/api/viewsets/webhook_viewset.py
+++ pedidos/api/viewsets/webhook_viewset.py
@@ -277,13 +277,13 @@ class StripeWebhookViewSet(ViewSet):
             #                      })
             
             if pedido:
-                valor_para_conta_conectada = pedido.total_split_stripe2
+                total_split = pedido.total_split_stripe
                 
                 ### Comentado 
-                #porcentagem_em_decimal = pedido.restaurante.pocentagem_para_tranferencia / 100
-                #taxa_atendimento = pedido.taxa_de_atendimento if pedido.taxa_de_atendimento else 0
-                #valor_para_conta_conectada = float(total_split) * float(porcentagem_em_decimal)
-                #valor_para_conta_conectada += float(taxa_atendimento)
+                porcentagem_em_decimal = pedido.restaurante.pocentagem_para_tranferencia / 100
+                taxa_atendimento = pedido.taxa_de_atendimento if pedido.taxa_de_atendimento else 0
+                valor_para_conta_conectada = float(total_split) * float(porcentagem_em_decimal)
+                valor_para_conta_conectada += float(taxa_atendimento)
                 ### Fim do comentario
 
                 # valor_para_conta_conectada /= 100  # Convertendo para reais
